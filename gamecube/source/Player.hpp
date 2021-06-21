cdf72a90604f8caf8d09ad1f02e815a9691a02eb
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-22 00:01:53 +0200

    CS - changes

---
+++ gamecube/source/Player.hpp
@@ -0,0 +1,29 @@
+// 
+// NGC
+// Player
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include "Sprite.hpp"
+
+class Player final: public Sprite {
+    private:
+        bool destroyed;
+
+    public:
+        Player() : Sprite(0, 0), destroyed(false) {};
+        Player(int x, int y) : Sprite(x, y), destroyed(false) {};
+        Player(int x, int y, int w, int h) : Sprite(x, y, w, h), destroyed(false) {};
+        Player(int x, int y, int w, int h, bool d) : Sprite(x, y, w, h), destroyed(d) {};
+    
+        void moveX(int dx);
+        void moveY(int dy);
+        void move(int dx, int dy);
+        void draw() override;
+        void draw(float d);
+
+        bool isDestroyed() const { return destroyed; };
+        void setDestroyed(bool d) { this->destroyed = d; };
+};
