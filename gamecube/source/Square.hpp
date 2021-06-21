2c7298725f26e38a6bef75b581239b5284f7d067
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-22 00:02:05 +0200

    CS - changes

---
+++ gamecube/source/Square.hpp
@@ -0,0 +1,26 @@
+//
+// NGC
+// Square
+// SchumakerTeam Lab.
+// Hudson Schumaker
+//
+
+#pragma once
+#include "Sprite.hpp"
+
+class Square final: public Sprite {
+    private:
+        bool destroyed;
+
+    public:
+        Square() : Sprite(0, 0), destroyed(false) {};
+        Square(int x, int y) : Sprite(x, y), destroyed(false) {};
+        Square(int x, int y, int w, int h) : Sprite(x, y, w, h), destroyed(false) {};
+        Square(int x, int y, int w, int h, bool d) : Sprite(x, y, w, h), destroyed(d) {};
+    
+        void draw() override;
+        void draw(float d);
+
+        bool isDestroyed() const { return destroyed; };
+        void setDestroyed(bool d) { this->destroyed = d; };
+};
