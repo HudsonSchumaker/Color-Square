9a94b728f57c26ed152b0c4cb2b7e204a301e6c2
Hudson Schumaker <hudson.schumaker@me.com>
2021-06-24 00:17:40 +0200

    CS - Player

--- gamecube/source/Player.cpp
+++ gamecube/source/Player.cpp
@@ -21,6 +21,11 @@ void Player::move(int dx, int dy) {
     this->y += dy;
 }
 
+void Player::setPos(int dx, int dy) {
+    this->x = dx;
+    this->y = dy;
+}
+
 void Player::draw() {
     GRRLIB_DrawImg(x, y, sprite, 0, 1, 1, Color::getWhite());
 }
