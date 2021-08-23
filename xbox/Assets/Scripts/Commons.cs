9260c0ce3101cf6a4518055a3e7e3cf5f94ad922
Hudson Schumaker <hudson.schumaker@me.com>
2021-08-23 19:52:32 +0200

    Xbox - Assets

---
+++ xbox/Assets/Scripts/Commons.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Commons
+{
+    public static int speed = 2;
+    public enum Colors { Grey, Red, Blue, Green, Yellow };
+
+}
