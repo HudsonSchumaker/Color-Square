d5efcb1d1aff01b9b2729b1a8673b36ededb4c5b
Hudson Schumaker <hudson.schumaker@me.com>
2021-08-23 19:53:23 +0200

    Xbox - Assets

---
+++ xbox/Assets/Scripts/ControllerMap.cs
@@ -0,0 +1,63 @@
+
+public class ControllerMap
+{
+    // Axes Menu
+    public const string HORIZONTAL_AXE = "Horizontal";
+    public const string HORIZONTAL_DPAD = "Horizontal_D-Pad";
+
+    public const string VERTICAL_AXE = "Vertical";
+    public const string VERTICAL_DPAD = "Vertical_D-Pad";
+
+    // Raw buttons Menu
+    public const string BUTTON_A = "A";
+    public const string BUTTON_B = "B";
+
+    // Functions Menu
+    public const string ACCEPT = BUTTON_A;
+    public const string BACK   = BUTTON_B;
+
+    // Axes Player 1
+    public const string HORIZONTAL_P1_AXE = "Horizontal_P1";
+    public const string VERTICAL_P1_AXE = "Vertical_P1";
+
+    public const string HORIZONTAL_P1_D_PAD = "Horizontal_P1_D-Pad";
+    public const string VERTICAL_P1_D_PAD = "Vertical_P1_D-Pad";
+
+    // Buttons Player 1
+    public const string BUTTON_A_P1 = "A_P1";
+    public const string BUTTON_B_P1 = "B_P1";
+
+    // Axes Player 2
+    public const string HORIZONTAL_P2_AXE = "Horizontal_P2";
+    public const string VERTICAL_P2_AXE = "Vertical_P2";
+
+    public const string HORIZONTAL_P2_D_PAD = "Horizontal_P2_D-Pad";
+    public const string VERTICAL_P2_D_PAD = "Vertical_P2_D-Pad";
+
+    // Buttons Player 2
+    public const string BUTTON_A_P2 = "A_P2";
+    public const string BUTTON_B_P2 = "B_P2";
+
+    // Axes Player 3
+    public const string HORIZONTAL_P3_AXE = "Horizontal_P3";
+    public const string VERTICAL_P3_AXE = "Vertical_P3";
+
+    public const string HORIZONTAL_P3_D_PAD = "Horizontal_P3_D-Pad";
+    public const string VERTICAL_P3_D_PAD = "Vertical_P3_D-Pad";
+
+    // Buttons Player 3
+    public const string BUTTON_A_P3 = "A_P3";
+    public const string BUTTON_B_P3 = "B_P3";
+
+    // Axes Player 4
+    public const string HORIZONTAL_P4_AXE = "Horizontal_P4";
+    public const string VERTICAL_P4_AXE = "Vertical_P4";
+
+    public const string HORIZONTAL_P4_D_PAD = "Horizontal_P4_D-Pad";
+    public const string VERTICAL_P4_D_PAD = "Vertical_P4_D-Pad";
+
+    // Buttons Player 4
+    public const string BUTTON_A_P4 = "A_P4";
+    public const string BUTTON_B_P4 = "B_P4";
+
+}
